836f6f462cdaae33b79ad8002ce56dc497d73975
henryliu <henryliuser@gmail.com>
2019-07-13 18:56:40 -0700

    enemies and their sprites

--- Scenes/Enemy.gd
+++ Scenes/Enemy.gd
@@ -8,14 +8,14 @@ func _ready():
 	gravity = Global.gravity
 
 func _physics_process(delta):
+	Global.dPrint(self,str(xDirec))
+	Global.dPrint(self,str(velocity))
 	imposeGravity()
 #	if Global.debug:
 #		Global.dPrint(self,str(velocity))
-	if velocity.x == 0:
+	if velocity.x <= 1:
 		velocity.x = speed*xDirec
 	velocity = move_and_slide(velocity, Global.UP)
-
-
 	if is_on_wall():
 		xDirec *= -1
 
@@ -36,6 +36,9 @@ func _on_Area2D_body_entered(body):
 			hurt()
 
 func hurt():
+	xDirec *= -1
+	velocity.x *= -1 
+
+func _on_Area2D_area_entered(area): #other enemy
+	xDirec *= -1
 	velocity.x *= -1
-	
-	
